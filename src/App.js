4edf98a07230de9e7cbc9bb84378a330f9fa29eb
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-10-28 21:34:01 +0700

    go to top

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import './App.css';
 import Header from './Components/Header.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BodyHome from './Components/BodyHome';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [showGoToTop, setShowGoToTop] = useState(false)
+
+  const handleScroll = () => {
+    if (window.scrollY > 1)
+    {
+      setShowGoToTop(true)
+    } else {
+      setShowGoToTop(false)
+    }
+  }
+
+  const handleGoToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <Router>
       <div className="app">
@@ -12,6 +33,13 @@ function App() {
           <Switch>
             <Route exact path="/"><BodyHome /></Route>
           </Switch>
+          {
+            showGoToTop && (
+              <button className="btn-gototop" onClick={handleGoToTop}>
+                <i class="btn-gototop-icon fas fa-arrow-up"></i>
+              </button>
+            )
+          }
         </div>
       </div>
     </Router>
