1c78cd5db38842d6bbd23baff85a56ae89a56a0b
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-11-29 13:00:04 +0700

    render phim

--- src/Components/BodyHomeMovie.js
+++ src/Components/BodyHomeMovie.js
@@ -1,9 +1,8 @@
 import React, {forwardRef, useState} from 'react';
 import './BodyHomeMovie.css';
-import wonderwoman from '../Assets/Images/woderwoman-12.png';
 import {Link} from 'react-router-dom';
 
-function BodyHomeMovie({name, age, type, description, item, handleBuyTicket}, ref) {
+function BodyHomeMovie({name, age, type, description, img, handleBuyTicket}, ref) {
 
   const handleClick = () => {
     const BodyHomeMore = document.querySelector('.bodyHomeMovie-more');
@@ -17,11 +16,11 @@ function BodyHomeMovie({name, age, type, description, item, handleBuyTicket}, re
         ref={ref}
         className="bodyHomeMovie" 
       >
-        <img src={wonderwoman} alt="#"/>
+        <img src={img} alt="#"/>
 
         <div className="bodyHomeMovie_infor">
           <h3>{name}</h3>
-          <h4><span>{age}</span>{type}<span>{item}</span></h4>
+          <h4><span>{age}</span>{type}</h4>
           <p>{description}</p>
           <button className="bodyHomeMovie_buy"><Link to="/datve"><i className="fas fa-cart-arrow-down"></i>Mua vé</Link></button>
           <button className="bodyHomeMovie_more" onClick={handleClick}><i className="fas fa-exclamation-circle"></i>Xem thêm</button>
--- src/Components/BodyHomeMovies.js
+++ src/Components/BodyHomeMovies.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './BodyHomeMovies.css';
 import BodyHomeMovie from './BodyHomeMovie';
+import axios from 'axios';
+import {findImage} from '../Images'
 
 function BodyHomeMovies({name}) {
   const [posters, setPosters] = useState([]);
@@ -8,9 +10,12 @@ function BodyHomeMovies({name}) {
   const [page, setPage] = useState(0);
 
   useEffect(() => {
-    // await axios.get(`/{name}`)
-    // .then((data) => setPosters(data.recordset));
-    setPosters([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]);
+    axios.get('http://localhost:5000/phim')
+    .then (data => {
+      if (data.data.length >= 1) setPosters([...data.data]);
+      else alert('Chưa có phim nào')
+    })
+    .catch(err => alert('Có lỗi xảy ra thử tải lại trang!'))
   },[]);
 
   useEffect(() => {
@@ -38,18 +43,18 @@ function BodyHomeMovies({name}) {
     <div className="bodyHomeMovies_wrap">
 
         <div className="bodyHomeMovies">
-          {arr.map((_, index) => (
+          {arr.map((item, index) => (
             <BodyHomeMovie
               key={index}
               padding={20} 
-              name="Raya and the last dragon" 
-              age={13} 
-              type="Trẻ em/Phiêu lưu"
-              description="Raya và Rồng Thần Cuối Cùng kể về một vương quốc huyền bí có tên là Kumandra – vùng đất mà loài rồng và con người sống hòa thuận với nhau. Nhưng rồi một thế lực đen tối bỗng đe dọa bình yên nơi đây, loài rồng buộc phải hi sinh để cứu lấy loài người..."  
+              name={item.TENPHIM}
+              age={item.DOTUOI} 
+              type={item.THELOAI}
+              description={item.MOTA}  
+              img = {findImage(item.TENPHIM)}
               height={300}
               width={207.7} 
               wrapWidth={20}
-              item={_}
             />
           ))}
         </div>
